90939cbffd00c12b8e0b841981d57701fb7aac65
ivancorderoc <ivan.corderocueva@gmail.com>
2023-04-16 20:16:31 +0200

    Initial commit

---
+++ utils/utilsEDA.py
@@ -0,0 +1,17 @@
+#Librerías
+#!pip install nfl-data-py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import nfl_data_py as nfl
+import seaborn as sns
+import warnings
+import os
+from scipy.stats import chi2_contingency
+warnings.filterwarnings("ignore")
+# ----------
+# Functions
+        
+        #Limpieza de datos
+
+# ----------
